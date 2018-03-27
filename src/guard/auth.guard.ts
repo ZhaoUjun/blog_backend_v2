14286912228e7d1314fc8306fd2345b963cf178a
ujun <zhaorujunfor@tianzehuitong.com>
2018-03-27 15:39:06 +0800

    authgard update

--- src/guard/auth.guard.ts
+++ src/guard/auth.guard.ts
@@ -1,10 +1,17 @@
-import {Guard, CanActivate, ExecutionContext} from '@nestjs/common';
+import {Guard, CanActivate, ExecutionContext, Inject} from '@nestjs/common';
 import 'reflect-metadata';
+import { REDIS_SCHEMA } from '../constant/index'
 
 @Guard()
 export class AuthGuard implements CanActivate {
+
+    constructor( @Inject('RedisProvider') private redisProvider){}
+
     async canActivate(req, context: ExecutionContext):Promise<boolean>{
-        return Reflect.hasMetadata('NoNeedLogin',context.parent)?true:
-            Reflect.hasMetadata('NoNeedLogin',context.handler)
+        if(Reflect.hasMetadata('NoNeedLogin',context.parent)||Reflect.hasMetadata('NoNeedLogin',context.handler)){
+            return true
+        }
+        const {account}=req.header;
+        return await this.redisProvider.get(REDIS_SCHEMA.JWT_TOKEN+':'+account)
     }
 }
--- src/utils/Response.ts
+++
@@ -1,5 +0,0 @@
-export class ResponseVo {
-    public data?:any;
-    public code?:number=0;
-    public msg?:string='成功'
-}
