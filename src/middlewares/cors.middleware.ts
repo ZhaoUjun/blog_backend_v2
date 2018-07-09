877dd4582247785ca1808441f809b34572c016bf
rujun <zhaorujunfor@tianzehuitong.com>
2018-07-09 22:49:24 +0800

    cors

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -6,7 +6,7 @@ export class CorsMiddleware implements NestMiddleware {
         return (req, res, next) => {
             res.header("Access-Control-Allow-Credentials", true);
             res.header("Access-Control-Allow-Origin", "http://119.23.255.113");
-            res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+            res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
             res.header("Access-Control-Allow-Headers", "X-Requested-With,Authorization,Sign,Time,Content-Type,APPId,AppKey");
             next();
         };
