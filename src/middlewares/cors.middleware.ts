40f1d576d7c41f198a22b746c62d0e35524fe2a4
rujun <zhaorujunfor@tianzehuitong.com>
2018-07-12 23:35:08 +0800

    migrate to nest 5.0.0

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -4,21 +4,9 @@ import { Injectable, NestMiddleware, MiddlewareFunction, Request, Response } fro
 export class CorsMiddleware implements NestMiddleware {
 	resolve(...args: any[]): MiddlewareFunction {
 		return (req: Request, res, next) => {
-		    if (req.method === 'OPTIONS'){
-                res.header('Access-Control-Allow-Credentials', true);
-                res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-                res.header(
-                    'Access-Control-Allow-Methods',
-                    'PUT, GET, POST, DELETE, OPTIONS',
-                );
-                res.header(
-                    'Access-Control-Allow-Headers',
-                    'X-Requested-With,Authorization,Sign,Time,Content-Type,APPId,AppKey',
-                );
-		        return res.end();
-            }
 			res.header('Access-Control-Allow-Credentials', true);
-			res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+            // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+            res.header('Access-Control-Allow-Origin', 'http://119.23.255.113');
 			res.header(
 				'Access-Control-Allow-Methods',
 				'PUT, GET, POST, DELETE, OPTIONS',
@@ -27,6 +15,9 @@ export class CorsMiddleware implements NestMiddleware {
 				'Access-Control-Allow-Headers',
 				'X-Requested-With,Authorization,Sign,Time,Content-Type,APPId,AppKey',
 			);
+            if (req.method === 'OPTIONS'){
+                return res.end();
+            }
 			next();
 		};
 	}
